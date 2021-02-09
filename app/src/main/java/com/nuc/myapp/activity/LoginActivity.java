2a500746ae9ffd8f65143039c4b4de7f9d3cc8ec
e <1151534558@qq.com>
2021-02-09 21:37:00 +0800

    登录注册接口

--- app/src/main/java/com/nuc/myapp/activity/LoginActivity.java
+++ app/src/main/java/com/nuc/myapp/activity/LoginActivity.java
@@ -1,29 +1,14 @@
 package com.nuc.myapp.activity;
+
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import com.nuc.myapp.R;
 import com.nuc.myapp.api.Api;
 import com.nuc.myapp.api.TtitCallback;
-import com.nuc.myapp.util.AppConfig;
 import com.nuc.myapp.util.StringUtil;
-import android.content.Intent;
-
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 
 public class LoginActivity extends BaseActivity {
@@ -67,6 +52,7 @@ public class LoginActivity extends BaseActivity {
         params.put("password",pwd);
 
         Api.config("app/login",params).postRequsr(new TtitCallback() {
+            //登录成功
             @Override
             public void onSuccess(final String res) {
                 runOnUiThread(new Runnable() {
@@ -76,10 +62,9 @@ public class LoginActivity extends BaseActivity {
                     }
                 });
             }
-
+            //登录失败
             @Override
             public void onFailure(Exception e) {
-
             }
         });
     }
--- app/src/main/java/com/nuc/myapp/activity/RegisterActivity.java
+++ app/src/main/java/com/nuc/myapp/activity/RegisterActivity.java
@@ -5,12 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nuc.myapp.R;
+import com.nuc.myapp.api.Api;
+import com.nuc.myapp.api.AppConfig;
+import com.nuc.myapp.api.TtitCallback;
+import com.nuc.myapp.util.StringUtil;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class RegisterActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+
+    public void register(String account , String pwd){
+
+        if (StringUtil.isEmpty(account)){
+            showToast("请输入账号");
+            return;
+        }
+        if (StringUtil.isEmpty(pwd)){
+            showToast("请输入密码");
+            return;
+        }
+
+        HashMap<String,Object> params = new HashMap<>();
+        params.put("mobile",account);
+        params.put("password",pwd);
+
+        Api.config(AppConfig.BASE_URI,params).postRequsr(new TtitCallback() {
+            @Override
+            public void onSuccess(final String res) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showToast(res);
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
     }
 }
